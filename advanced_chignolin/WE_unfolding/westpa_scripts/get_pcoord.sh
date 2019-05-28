6fb37738ecc4b315de74bf2fa275ef69b751438e
barmakmostofian <barmakmostofian@users.noreply.github.com>
2019-05-28 15:39:05 -0700

    Update get_pcoord.sh

--- advanced_chignolin/WE_unfolding/westpa_scripts/get_pcoord.sh
+++ advanced_chignolin/WE_unfolding/westpa_scripts/get_pcoord.sh
@@ -6,19 +6,10 @@ if [ -n "$SEG_DEBUG" ] ; then
 fi
 
 cd $WEST_SIM_ROOT
-echo 'I am here'
-cp chignolin.rst7 parent.rst7
-function cleanup() {
-    rm -f parent.rst7
-}
 
-trap cleanup EXIT
-
-# Get progress coordinate
 cpptraj common_files/chignolin.prmtop <ptraj_rmsd_init.in || exit 1
-gawk '{print $2}' rmsd.temp | tail -1 > pcoord.dat || exit 1
-cat pcoord.dat > $WEST_PCOORD_RETURN
-rm mdinfo rmsd.temp eted.dat pcoord*
+gawk '{print $2}' rmsd.temp | tail -1 > $WEST_PCOORD_RETURN || exit 1
+rm  rmsd.temp
 
 if [ -n "$SEG_DEBUG" ] ; then
     head -v $WEST_PCOORD_RETURN
