beae20b2d96d75614c0bf3f19f9a000fd0d1223e
barmakmostofian <barmakmostofian@users.noreply.github.com>
2019-05-30 17:13:23 -0700

    Update get_pcoord.sh

--- advanced_chignolin/WE_unfolding/westpa_scripts/get_pcoord.sh
+++ advanced_chignolin/WE_unfolding/westpa_scripts/get_pcoord.sh
@@ -9,7 +9,7 @@ cd $WEST_SIM_ROOT
 
 cpptraj common_files/chignolin.prmtop <get_rmsd_init.in || exit 1
 gawk '{print $2}' rmsd_init.dat | tail -1 > $WEST_PCOORD_RETURN || exit 1
-rm  rmsd_init.dat 
+rm -rf rmsd_init.dat 
 
 if [ -n "$SEG_DEBUG" ] ; then
     head -v $WEST_PCOORD_RETURN
