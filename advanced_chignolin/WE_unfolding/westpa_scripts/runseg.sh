8fc49894ed1065a5dadc51a96971323158114c01
barmakmostofian <barmakmostofian@users.noreply.github.com>
2019-05-30 17:11:39 -0700

    Update runseg.sh

--- advanced_chignolin/WE_unfolding/westpa_scripts/runseg.sh
+++ advanced_chignolin/WE_unfolding/westpa_scripts/runseg.sh
@@ -10,27 +10,19 @@ mkdir -pv $WEST_CURRENT_SEG_DATA_REF || exit 1
 cd $WEST_CURRENT_SEG_DATA_REF || exit 1
 
 
-ln -sv $WEST_SIM_ROOT/{reference.pdb,chignolin.prmtop,ptraj_rmsd.in} .
+ln -sv $WEST_SIM_ROOT/common_files/{chignolin.pdb,chignolin.prmtop} .
 
-case $WEST_CURRENT_SEG_INITPOINT_TYPE in
-    SEG_INITPOINT_CONTINUES)
-        ln -sv $WEST_PARENT_DATA_REF/seg.rst7 ./parent.rst7
-        ln -sv $WEST_SIM_ROOT/md-continue.in md.in 
-    ;;
+if [ "$WEST_CURRENT_SEG_INITPOINT_TYPE" = "SEG_INITPOINT_CONTINUES" ]; then
+  sed "s/RAND/$WEST_RAND16/g" $WEST_SIM_ROOT/common_files/md.in > md.in
+  ln -sv $WEST_PARENT_DATA_REF/seg.rst ./parent.rst
+elif [ "$WEST_CURRENT_SEG_INITPOINT_TYPE" = "SEG_INITPOINT_NEWTRAJ" ]; then
+  sed "s/RAND/$WEST_RAND16/g" $WEST_SIM_ROOT/common_files/md.in > md.in
+  ln -sv $WEST_SIM_ROOT/common_files/chignolin.rst parent.rst
+fi
 
-    SEG_INITPOINT_NEWTRAJ)
-        ln -sv $WEST_SIM_ROOT/md-genvel.in md.in
-        ln -sv $WEST_SIM_ROOT/chignolin.rst7 parent.rst7
-    ;;
-
-    *)
-        echo "unknown init point type $WEST_CURRENT_SEG_INITPOINT_TYPE"
-        exit 2
-    ;;
-esac
 
 pmemd -O  -i md.inp  -p chignolin.prmtop  -c parent.rst  -x seg.mdcrd  -r seg.rst  -o seg.out  -inf seg.info   || exit 1
 
-cpptraj chignolin.prmtop <ptraj_rmsd.in || exit 1
-gawk '{print $2}' rmsd.temp | tail -2 > $WEST_PCOORD_RETURN || exit 1
+cpptraj chignolin.prmtop < $WEST_SIM_ROOT/get_rmsd.in || exit 1
+awk '{print $2}' rmsd.dat | tail -2 > $WEST_PCOORD_RETURN || exit 1
 
