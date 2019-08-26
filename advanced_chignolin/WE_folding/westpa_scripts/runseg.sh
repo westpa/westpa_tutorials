023b07ae87d3ea48f6716e373f0f84607f26793d
barmakmostofian <barmakmostofian@users.noreply.github.com>
2019-08-25 17:30:12 -0700

    Update runseg.sh

--- advanced_chignolin/WE_folding/westpa_scripts/runseg.sh
+++ advanced_chignolin/WE_folding/westpa_scripts/runseg.sh
@@ -19,7 +19,7 @@ elif [ "$WEST_CURRENT_SEG_INITPOINT_TYPE" = "SEG_INITPOINT_NEWTRAJ" ]; then
   ln -sv $WEST_PARENT_DATA_REF ./parent.rst
 fi
 
-$PMEMD -O -i md.in   -p chignolin.prmtop  -c parent.rst \
+$SANDER -O -i md.in   -p chignolin.prmtop  -c parent.rst \
           -r seg.rst -x seg.nc      -o seg.log    -inf seg.nfo
 
 TEMP=$(mktemp)
