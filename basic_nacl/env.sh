33d4bb4fa169c4dbd07d03655d0889cdc7dc8cd1
atbogetti <atb43@pitt.edu>
2019-10-12 17:50:43 -0400

    fixed openmm environment issues

--- basic_nacl/env.sh
+++ basic_nacl/env.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Set up environment for dynamics
-source /home/atb43/apps/amber18/amber.sh
-
 # Set up environment for westpa
-source /home/atb43/apps/westpa/westpa.sh
 export WEST_PYTHON=$(which python2.7)
+source activate westpa-2017.10
 export WEST_SIM_ROOT="$PWD"
 export SIM_NAME=$(basename $WEST_SIM_ROOT)
