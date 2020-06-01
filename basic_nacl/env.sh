fe44f00362a7d1553c2b35187eeae247e944da43
Joshua L. Adelman <joshua.adelman@gmail.com>
2020-06-01 15:29:52 -0400

    Initial work updating basic_nacl tutorial for westpa restructuring

--- basic_nacl/env.sh
+++ basic_nacl/env.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Set up environment for westpa
-export WEST_PYTHON=$(which python2.7)
-# Actviate a conda environment containing westpa, openmm, mdtraj;
-# You may need to create this first (see install instructions)
-source activate westpa-openmm-mdtraj
 export WEST_SIM_ROOT="$PWD"
 export SIM_NAME=$(basename $WEST_SIM_ROOT)
--- basic_nacl/init.sh
+++ basic_nacl/init.sh
@@ -12,7 +12,7 @@ BSTATE_ARGS="--bstate-file $WEST_SIM_ROOT/bstates/bstates.txt"
 TSTATE_ARGS="--tstate-file $WEST_SIM_ROOT/tstate.file"
 
 # Run w_init
-$WEST_ROOT/bin/w_init \
+w_init \
   $BSTATE_ARGS \
   $TSTATE_ARGS \
   --segs-per-state 5 \
