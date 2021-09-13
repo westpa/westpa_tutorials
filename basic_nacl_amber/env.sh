c71db43b38cf855dda70192b50cb98eede1d24c5
Anthony Bogetti <anthony.bogetti@gmail.com>
2021-09-13 17:16:42 -0400

    fixed env.sh to not use python2

--- basic_nacl_amber/env.sh
+++ basic_nacl_amber/env.sh
@@ -4,8 +4,8 @@
 source /home/atb43/apps/amber18/amber.sh
 
 # Set up environment for westpa
-source /home/atb43/apps/westpa/westpa.sh
-export WEST_PYTHON=$(which python2.7)
+#source /home/atb43/apps/westpa/westpa.sh
+#export WEST_PYTHON=$(which python2.7)
 export WEST_SIM_ROOT="$PWD"
 export SIM_NAME=$(basename $WEST_SIM_ROOT)
 
