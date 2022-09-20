948c6ee0561480a8f4ea07ea5441e5fe340dff9e
Anthony Bogetti <anthony.bogetti@gmail.com>
2022-09-20 10:36:49 -0400

    updated namd tutorial to WESTPA 2.0 (first draft)

--- additional_tutorials/basic_nacl_namd/env.sh
+++ additional_tutorials/basic_nacl_namd/env.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Set up environment for dynamics
-source $AMBERHOME/amber.sh
-
 # Set WESTPA-related variables
 export WEST_SIM_ROOT="$PWD"
 export SIM_NAME=$(basename $WEST_SIM_ROOT)
 
 # Set runtime commands
-export PMEMD=$(which pmemd)
-export CPPTRAJ=$(which cpptraj)
+export NAMD=$(which namd2)
--- additional_tutorials/basic_nacl_namd/init.sh
+++ additional_tutorials/basic_nacl_namd/init.sh
@@ -7,13 +7,35 @@ source env.sh
 rm -rf traj_segs seg_logs istates west.h5 
 mkdir   seg_logs traj_segs istates
 
-# Set pointer to bstate and tstate
-BSTATE_ARGS="--bstate-file $WEST_SIM_ROOT/bstates/bstates.txt"
-TSTATE_ARGS="--tstate-file $WEST_SIM_ROOT/tstate.file"
+# Make sure the CHARMM force field is in the right spot:
+if [ ! -d common_files/toppar ]; then
+  echo "CHARMM force field not found! Download the CHARMM36 force field, untar "
+  echo "it, and place the 'toppar' directory in namd_config."
+  exit 1
+fi
 
-# Run w_init
+# Process the CHARMM force field file to be compatibile with NAMD
+cat common_files/toppar/toppar_water_ions.str \
+  | grep -v -e "^set" \
+  | grep -v -e "^if" \
+  | grep -v -e "^WRNLEV"\
+  | grep -v -e "^BOMLEV"\
+  | grep -v -e "^SOD\s\s\s\sO"\
+  > common_files/toppar/toppar_water_ions_for_namd.str
+
+# Define the arguments for the basis states (used for generating initial 
+# states; in this case we only have one), and target states (used for
+# knowing when to recycle trajectories). In this example, we recycle
+# trajectories as they reach the bound state; we focus on sampling  
+# the binding process (and not the unbinding process).
+
+BSTATE_ARGS="--bstate-file bstates/bstates.txt"
+TSTATE_ARGS="--tstate bound,1.0"
+
+# Initialize the simulation, creating the main WESTPA data file (west.h5)
+# The "$@" lets us take any arguments that were passed to init.sh at the
+# command line and pass them along to w_init.
 w_init \
-  $BSTATE_ARGS \
-  $TSTATE_ARGS \
-  --segs-per-state 1 \
+  $BSTATE_ARGS $TSTATE_ARGS \
+  --segs-per-state 5 \
   --work-manager=threads "$@"
