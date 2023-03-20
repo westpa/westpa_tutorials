e9cacafa0e2b63ec3a8c518a2aac94b1c8f687bb
Jeremy Leung <jml230@pitt.edu>
2023-03-20 15:56:31 -0400

    cpptraj typo for amber tutorials

--- additional_tutorials/basic_nacl_amber/westpa_scripts/runseg.sh
+++ additional_tutorials/basic_nacl_amber/westpa_scripts/runseg.sh
@@ -26,7 +26,7 @@ TEMP=$(mktemp)
 COMMAND="         parm nacl.parm7\n"
 COMMAND="$COMMAND trajin $WEST_CURRENT_SEG_DATA_REF/parent.rst\n"
 COMMAND="$COMMAND trajin $WEST_CURRENT_SEG_DATA_REF/seg.nc\n"
-COMMAND="$COMMAND autoimage fixed Na+ \n"
+COMMAND="$COMMAND autoimage fixed :1@Na+ \n"
 COMMAND="$COMMAND distance na-cl :1@Na+ :2@Cl- out $TEMP\n"
 COMMAND="$COMMAND go\n"
 
@@ -38,7 +38,7 @@ if [ ${WEST_COORD_RETURN} ]; then
   COMMAND="$COMMAND trajin  $WEST_CURRENT_SEG_DATA_REF/parent.rst\n"
   COMMAND="$COMMAND trajin  $WEST_CURRENT_SEG_DATA_REF/seg.nc\n"
   COMMAND="$COMMAND strip :WAT \n"
-  COMMAND="$COMMAND autoimage fixed Na+ \n"
+  COMMAND="$COMMAND autoimage fixed :1@Na+ \n"
   COMMAND="$COMMAND trajout $WEST_CURRENT_SEG_DATA_REF/seg.pdb\n"
   COMMAND="$COMMAND go\n"
   echo -e $COMMAND | $CPPTRAJ 
