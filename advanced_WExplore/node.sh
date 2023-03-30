4b3df45d0edebe0a4dae58eae3f66f9b6c142ef3
Jeremy Leung <jml230@pitt.edu>
2023-03-29 22:01:46 -0400

    reorganize tutorials to match combo tutorials

--- advanced_WExplore/node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-set -x
-
-cd $SLURM_SUBMIT_DIR
-(source env.sh
-cd $WEST_SIM_ROOT
-
-w_run "$@" ) &> west-node.log
--- advanced_WExplore/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source env.sh
-
-rm -f west.log
-w_run "$@"  &> west.log
