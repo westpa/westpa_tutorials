7696f1e633cff0532c7bea8cbc7d43f219a11d38
Anthony Bogetti <anthony.bogetti@gmail.com>
2023-08-16 14:38:32 -0400

    added nacl_gmx tutorial

---
+++ additional_tutorials/basic_nacl_gmx/env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set up environment for dynamics
+source /usr/local/gromacs/bin/GMXRC
+
+# Set WESTPA-related variables
+export WEST_SIM_ROOT="$PWD"
+export SIM_NAME=$(basename $WEST_SIM_ROOT)
+
+# Set runtime commands
+export GMX=$(which gmx_mpi)
