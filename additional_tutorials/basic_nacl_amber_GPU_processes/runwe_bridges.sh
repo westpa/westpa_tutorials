4cfb47377dd0d081dc425910271e4032a4a985ea
Jeremy Leung <jml230@pitt.edu>
2023-09-08 15:01:49 -0400

    final fixes to GPU_processes

--- additional_tutorials/basic_nacl_amber_GPU_processes/runwe_bridges.sh
+++ additional_tutorials/basic_nacl_amber_GPU_processes/runwe_bridges.sh
@@ -8,12 +8,13 @@
 #SBATCH --gres=gpu:4
 #SBATCH --time=24:00:00
 
-set -x
-cd $SLURM_SUBMIT_DIR
+#set -x
+#cd $SLURM_SUBMIT_DIR
 source env.sh || exit 1
 
 env | sort
 
 cd $WEST_SIM_ROOT
 
+# The number of workers here should match the number of GPU you request above.
 w_run --work-manager=processes --n-workers=4 "$@" &> west-$SLURM_JOBID.log
